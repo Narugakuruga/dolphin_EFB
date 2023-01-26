0f803417cde7ff259e3422c2fe92261a294633b2
Narugakuruga <31060534+Narugakuruga@users.noreply.github.com>
2023-01-26 09:06:33 +0800

    Update GraphicsSettings.h

--- Source/Core/Core/Config/GraphicsSettings.h
+++ Source/Core/Core/Config/GraphicsSettings.h
@@ -143,11 +143,13 @@ extern const Info<bool> GFX_HACK_NO_MIPMAPPING;
 #endif
 
 // Graphics.Hacks EFB Scale exclusion settings
-extern const Info<bool> GFX_EFB_BLOOM_FIX_ENABLED;
-extern const Info<int> GFX_EFB_BLOOM_FIX_WIDTH;
-extern const Info<bool> GFX_EFB_BLOOM_FIX_DOWNSCALE;
-extern const Info<bool> GFX_EFB_BLOOM_FIX_ALT_FILTER;
-extern const Info<bool> GFX_EFB_BLOOM_FIX_SKIP_FIRST;
+extern const Info<int> GFX_EFB_SCALE_EXCLUDE_WIDTH;
+extern const Info<int> GFX_EFB_SCALE_EXCLUDE_BLOOM_STRENGTH;
+extern const Info<int> GFX_EFB_SCALE_EXCLUDE_BLUR_RADIUS;
+extern const Info<bool> GFX_EFB_SCALE_EXCLUDE_ALT;
+extern const Info<bool> GFX_EFB_SCALE_EXCLUDE_BLUR;
+extern const Info<bool> GFX_EFB_SCALE_EXCLUDE_DOWNSCALE;
+extern const Info<bool> GFX_EFB_SCALE_EXCLUDE_ENABLED;
 
 // Graphics.GameSpecific
 
